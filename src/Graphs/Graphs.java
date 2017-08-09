d74625e9f9c08f2f9af7e87c57b86e6134fa7f94
Damian <damianszkudlarek@gmail.com>
2017-08-09 20:27:30 +0200

    * Graphs - Transform E to A * Graphs - Transform A to E

--- src/Graphs/Graphs.java
+++ src/Graphs/Graphs.java
@@ -26,15 +26,108 @@ public class Graphs {
 //            System.out.println(e.toString());
 //        }
 
-        int n = 4;
-        int k = 6;
-        Edge[] resultGnk = generateGnk(n,k);
+        /*
+        Generator Gnk
+         */
+//        int n = 4;
+//        int k = 6;
+//        Edge[] resultGnk = generateGnk(n,k);
+//
+//        if( k <= resultGnk.length ) {
+//            for (int i = resultGnk.length - 1; i >= resultGnk.length - k; i--) {
+//                System.out.println(resultGnk[i].toString());
+//            }
+//        }
 
-        if( k <= resultGnk.length ) {
-            for (int i = resultGnk.length - 1; i >= resultGnk.length - k; i--) {
-                System.out.println(resultGnk[i].toString());
+
+        /*
+        * Transform E to A
+        * */
+//        int n = 4;
+//        int k = 4;
+//        Edge[] resultGnk = generateGnk(n,k);
+//        boolean[][] result = transformEtoA(n, resultGnk, k);
+//
+//        for(int i =0; i < result.length; i++){
+//            System.out.println(Arrays.toString(result[i]));
+//        }
+//        for (int i = resultGnk.length - 1; i >= resultGnk.length - k; i--) {
+//            System.out.println(resultGnk[i].toString());
+//        }
+
+
+        /* Transform A to E */
+        int n = 4;
+        double p = 0.5;
+
+        boolean[][] a = generateGNP(n, p);
+        int k = countEdgesInA(a, n);
+        Edge[] result = transformAtoE(a, n);
+        for (int i = result.length - 1; i >= result.length - k; i--) {
+            System.out.println(result[i].toString());
+        }
+    }
+
+    private static int countEdgesInA(boolean[][] a, int n) {
+        int result = 0;
+        /* Przelatujemy po prawym górnym trójkącie w tablicy A */
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if(a[i][j]) {
+                    result++;
+                }
             }
         }
+        return result;
+    }
+
+    private static Edge[] transformAtoE(boolean[][] a, int n) {
+        /* wygenerowanie tablicy z wszystkimi możliwymi krawędziami */
+        Edge[] result = generateEdgeArray(n);
+        /* index z którym zamieniamy daną krawędź, po każdej zamianie
+        zmniejszamy go 1 = zmniejsząc zakres nieprzesuniętych elementów
+         */
+        int index = result.length - 1;
+
+        /* Przelatujemy po strukturze A, czyli po "prawym trójkącie" */
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if(a[i][j]) {
+                    /* zamiana istniejącej krawędzi z ostatnią z nieprzesuniętych */
+                    result = moveItem(result, i, j, index);
+                    /* zmniejszamy zakres nieprzesuniętych elementów w results */
+                    index--;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static Edge[] moveItem(Edge[] result, int i, int j, int index)
+    {
+        for (Edge e: result) {
+            /* jeśli znajdziemy w E krawędź {i:j} */
+            if (e.a == i && e.b == j) {
+                /* to zamieniamy ją z ostatnim nieprzesuniętym elementem */
+                Edge temp = e;
+                e = result[index];
+                result[index] = temp;
+            }
+        }
+        return result;
+    }
+
+    private static boolean[][] transformEtoA(int n, Edge[] struct, int k)
+    {
+        boolean[][] result = new boolean[n][n];
+        int h = struct.length;
+
+        for(int i = h-1; i > h-1 -k;i--) {
+            result[struct[i].a][struct[i].b] = true;
+            result[struct[i].b][struct[i].a] = true;
+        }
+
+        return result;
     }
 
     private static Edge[] generateGnk(int n, int k) {
